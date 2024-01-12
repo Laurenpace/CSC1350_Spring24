aaae681c04396fcd36da6d4e2be960a3774c405f
Lauren Pace <laurenpace99@gmail.com>
2024-01-12 16:09:45 -0600

    init helloworld

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,2 +1,7 @@
-package CSC1350_Spring24;public class HelloWorld {
+package CSC1350_Spring24;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
 }
