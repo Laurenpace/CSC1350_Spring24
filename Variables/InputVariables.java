75fe4709d0d73bf79a903f9d7fa4a17bbb363e8b
Lauren Pace <laurenpace99@gmail.com>
2024-02-06 15:56:07 -0600

    1/30

--- Variables/InputVariables.java
+++ Variables/InputVariables.java
@@ -5,8 +5,17 @@ import java.util.Scanner;
 public class InputVariables {
     public static void main(String[] args) {
         Scanner in = new Scanner(System.in);
+        System.out.print("Enter a line of text: " );
+        String str = in.nextLine();
+
         System.out.print("Give me a double number: ");
         double num = in.nextDouble();
         System.out.println("You entered " + num);
+
+        System.out.print("Give me an integer number: ");
+        int numInt = in.nextInt();
+        System.out.println("You entered " + numInt);
+
+
     }
 }
