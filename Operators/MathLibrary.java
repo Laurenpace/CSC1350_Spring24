22f0b901c065f239ff76007c186dd5f19eb059d3
Lauren Pace <laurenpace99@gmail.com>
2024-02-19 14:03:25 -0600

    2/6

--- Operators/MathLibrary.java
+++ Operators/MathLibrary.java
@@ -3,6 +3,7 @@ package CSC1350_Spring24.Operators;
 public class MathLibrary {
     public static void main(String[] args) {
         System.out.println(Math.pow(2, 2));
-//        "str".
+        double var = Math.abs(-4);
+        System.out.println(var);
     }
 }
