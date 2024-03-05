16f0bf7d90d17ee45315f490bbdbfe9ada15add0
Lauren Pace <laurenpace99@gmail.com>
2024-03-05 12:25:16 -0600

    3/5

--- Loops/Random.java
+++ Loops/Random.java
@@ -1,10 +1,29 @@
 package CSC1350_Spring24.Loops;
 
+import java.text.DecimalFormat;
+
 public class Random {
     public static void main(String[] args) {
         System.out.println(Math.random());
 
-        System.out.println((int)(Math.random() * 10)  + 1);
+        System.out.println((Math.random() * 100)  + 1);
+        System.out.println((Math.random() * 100)  + 1);
+        System.out.println((Math.random() * 100)  + 1);
+
+        //ways to round number to n (4 here) decimal places
+        double randomNum = Math.random();
+        System.out.println(randomNum);
+        double number = Math.round(randomNum * 100 * Math.pow(10, 4)) / Math.pow(10, 4);
+        System.out.println(number);
+        DecimalFormat df_obj = new DecimalFormat("#.####");
+        System.out.println(df_obj.format(number));
+
+
+        System.out.println((int)(Math.random() * 100)  + 1);
+        System.out.println((int)(Math.random() * 100)  + 1);
+        System.out.println((int)(Math.random() * 100)  + 1);
+        System.out.println((int)(Math.random() * 100)  + 1);
+        System.out.println((int)(Math.random() * 100)  + 1);
         System.out.println((int)(Math.random() * 10)  + 1);
         System.out.println((int)(Math.random() * 10)  + 1);
         System.out.println((int)(Math.random() * 10)  + 1);
